1fb0955067b71735eff63ed7476623e3dcb11a45
Mihai Petrenco <mihaipetrenco@icloud.com>
2022-10-09 18:49:43 +0300

    Added project files and

---
+++ README.md
@@ -0,0 +1,9 @@
+# WeatherApp
+
+A weather forecast application developed using `SwiftUI` and `TCA`.
+
+## Features:
+- _To add..._
+
+## Architecture Overview:
+- _To add..._
---
+++ WeatherApp/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Mihai Petrenco on 09.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Color.yellow.ignoresSafeArea()
+        }
+    }
+}
