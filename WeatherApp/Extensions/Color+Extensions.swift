946a7206c6a5b78b31e57e07f80e15b6db68ee33
Mihai Petrenco <mihaipetrenco@icloud.com>
2022-10-09 18:55:16 +0300

    Added image and color extensions for easy access to custom assets

---
+++ WeatherApp/Extensions/Color+Extensions.swift
@@ -0,0 +1,15 @@
+//
+//  Color+Extensions.swift
+//  WeatherApp
+//
+//  Created by Mihai Petrenco on 09.10.2022.
+//
+
+import SwiftUI
+
+extension Color {
+    static let appText = Color("App Text")
+    static let weatherSunny = Color("Weather Sunny")
+    static let weatherCloudy = Color("Weather Cloudy")
+    static let weatherRainy = Color("Weather Rainy")
+}
---
+++ WeatherApp/Extensions/Image+Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  Image+Extensions.swift
+//  WeatherApp
+//
+//  Created by Mihai Petrenco on 09.10.2022.
+//
+
+import SwiftUI
+
+extension Image {
+    static let forecastSunny = Image("Forecast Sunny")
+    static let forecastCloudy = Image("Forecast Cloudy")
+    static let forecastRainy = Image("Forecast Rainy")
+}
