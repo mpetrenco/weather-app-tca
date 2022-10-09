33ff4ef2fefaa046e3c0d84fc65861360b3ff629
Mihai Petrenco <mihaipetrenco@icloud.com>
2022-10-09 19:06:55 +0300

    Added the `OpenWeatherService` implementation

--- WeatherApp/Networking/WeatherService/OpenWeatherService.swift
+++ WeatherApp/Networking/WeatherService/OpenWeatherService.swift
@@ -7,28 +7,75 @@
 
 import Foundation
 
-enum URLRequestError: Error {
-    case unknown
+private enum OpenWeatherEndpoint: String {
+    case currentWeather = "weather"
+    case forecast = "forecast"
 }
 
-struct OpenWeatherService: WeatherService {
+public struct OpenWeatherService: WeatherService {
     
-    func fetchCurrentWeather(
+    // MARK: - Properties
+    
+    private let apiKey: String
+    
+    // MARK: - Initializers
+    
+    public init(with apiKey: String) {
+        self.apiKey = apiKey
+    }
+    
+    // MARK: - Protocol Methods
+    
+    public func fetchCurrentWeather(
         latitude: Double,
         longitude: Double
     ) async -> Result<Weather, Error> {
         
-        // TODO: Implement method
-        return .failure(URLRequestError.unknown)
+        await request(
+            endpoint: .currentWeather,
+            latitude: latitude,
+            longitude: longitude,
+            expecting: Weather.self
+        )
     }
     
-    func fetchForecast(
+    public func fetchForecast(
         latitude: Double,
         longitude: Double
     ) async -> Result<Forecast, Error> {
         
-        // TODO: Implement method
-        return .failure(URLRequestError.unknown)
+        await request(
+            endpoint: .forecast,
+            latitude: latitude,
+            longitude: longitude,
+            expecting: Forecast.self
+        )
     }
     
+    // MARK: - Helper Methods
+    
+    private func request<T: Codable>(
+        endpoint: OpenWeatherEndpoint,
+        latitude: Double,
+        longitude: Double,
+        expecting type: T.Type
+    ) async -> Result<T, Error> {
+        
+        var components = URLComponents(
+            string: "https://api.openweathermap.org/data/2.5/\(endpoint.rawValue)"
+        )
+        
+        components?.queryItems = [
+            URLQueryItem(name: "appid", value: apiKey),
+            URLQueryItem(name: "lat", value: "\(latitude)"),
+            URLQueryItem(name: "lon", value: "\(longitude)"),
+            URLQueryItem(name: "units", value: "metric"),
+        ]
+        
+        return await URLSession.shared.request(
+            url: components?.url,
+            expecting: type.self
+        )
+    }
 }
+
